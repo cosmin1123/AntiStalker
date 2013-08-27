7eb2ded2eaf73275333fa55ea9d938dd25139216
Flavius Anton <fanton@rosedu.org>
2013-08-27 10:09:52 +0300

    Second initial commit.

---
+++ src/com/example/antistalker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.antistalker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Toast.makeText(this, "Hello, Stalker!", Toast.LENGTH_LONG).show();
+    }
+}
