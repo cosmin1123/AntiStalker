d0c7e33e7b270f4044796c904770d556a1d8fe34
Flavius Anton <fanton@rosedu.org>
2013-08-27 15:33:06 +0300

    Add blocked list.

--- src/com/example/antistalker/Person.java
+++ src/com/example/antistalker/Person.java
@@ -1,5 +1,8 @@
 package com.example.antistalker;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created with IntelliJ IDEA.
  * User: cosmin
@@ -7,12 +10,44 @@ package com.example.antistalker;
  * Time: 10:13 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Person {
+public class Person implements Parcelable {
     String name;
     String telephone;
+
+    public static final Parcelable.Creator<Person> CREATOR
+            = new Parcelable.Creator<Person>() {
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
 
     public Person(String name, String telephone){
         this.name = name;
         this.telephone = telephone;
+    }
+
+    public Person (Parcel source) {
+        name = source.readString();
+        telephone = source.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(telephone);
+    }
+
+    @Override
+    public String toString() {
+        return new String(name + "    " + telephone);
     }
 }
--- src/com/example/antistalker/ViewBlockedActivity.java
+++ src/com/example/antistalker/ViewBlockedActivity.java
@@ -1,7 +1,13 @@
 package com.example.antistalker;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,5 +22,14 @@ public class ViewBlockedActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blockedview);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        List<Person> blockedPersons = (List<Person>) getIntent().getExtras().get("blockedPersons");
+
+        Log.v("TAG", "Super Persons: " + blockedPersons);
+        ArrayAdapter<Person> adapter = new ArrayAdapter<Person>(this, R.layout.textview, blockedPersons);
+
+        listView.setAdapter(adapter);
     }
 }
